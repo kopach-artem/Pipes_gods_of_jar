6dbb23036d102f7920ef580d81804da5c41a8b20
AO7JX4 <zuiop21.lol@gmail.com>
2023-04-17 01:05:44 +0200

    Main

--- Pipeline_Game/src/skeleton/Main.java
+++ Pipeline_Game/src/skeleton/Main.java
@@ -5,32 +5,31 @@ import java.util.Scanner;
 
 public class Main
 {
-    
-    /** 
-     * @param args
-     * @throws MyException
-     * @throws InterruptedException
-     */
+
+    static void printOperations()
+    {
+        System.out.println("Válassza ki a kívánt szekvenciát!");
+        System.out.println("1. Mehcanic repairs pump");
+        System.out.println("2. Mechanic repairs pipe");
+        System.out.println("3. Saboteur leaks pipe");
+        System.out.println("4. Player attach pipe");
+        System.out.println("5. Player attaches pump");
+        System.out.println("6. Player detach pipe");
+        System.out.println("7. Player adjust pump Input");
+        System.out.println("8. Player adjust pump Output");
+        System.out.println("9. Player moves to pipe succesfully");
+        System.out.println("10. Player moves to pipe fail");
+        System.out.println("11. Player moves to pump");
+        System.out.println("12. Player moves to cistern");
+        System.out.println("13. Player moves to mountain spring");
+        System.out.println("14. Random pump breaking");
+        System.out.println("15. Collecting water in cistern");
+        System.out.println("0. Kilépés");
+    }
     public static void main(String[] args) throws MyException, InterruptedException {
         DispatcherSkeleton skeleton = new DispatcherSkeleton();
-        System.out.println("[Üdvözli önt a Gods of jar csapat skeleton programja!]");
-        System.out.println("[Válassza ki a kívánt szekvenciát!]");
-        System.out.println("[1]. Mehcanic repairs pump");
-        System.out.println("[2]. Mechanic repairs pipe");
-        System.out.println("[3]. Saboteur leaks pipe");
-        System.out.println("[4]. Player attach pipe");
-        System.out.println("[5]. Player attaches pump");
-        System.out.println("[6]. Player detach pipe");
-        System.out.println("[7]. Player adjust pump Input");
-        System.out.println("[8]. Player adjust pump Output");
-        System.out.println("[9]. Player moves to pipe succesfully");
-        System.out.println("[10]. Player moves to pipe fail");
-        System.out.println("[11]. Player moves to pump");
-        System.out.println("[12]. Player moves to cistern");
-        System.out.println("[13]. Player moves to mountain spring");
-        System.out.println("[14]. Random pump breaking");
-        System.out.println("[15]. Collecting water in cistern");
-        System.out.println("[0]. Kilépés");
+        System.out.println("Üdvözli önt a Gods of jar csapat skeleton programja!");
+        printOperations();
         int n;
         Scanner scanner = new Scanner(System.in);
         while ((n = scanner.nextInt()) != 0) {
@@ -81,8 +80,6 @@ public class Main
                     skeleton.CollectingWater();
                     break;
             }
-
         }
-        scanner.close();
     }
 }
